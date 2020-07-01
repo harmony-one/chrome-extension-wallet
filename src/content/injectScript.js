f5ba24edcd7630b4ba629e20b4dfc712e3a5a3f4
Raptor <blueraptor1001@gmail.com>
2020-07-01 08:01:00 -0700

    add inject-script for third party apps

--- src/content/injectScript.js
+++ src/content/injectScript.js
@@ -1,8 +1,8 @@
-import walletController from "../services/walletController";
+import walletProvider from "./WalletProvider";
 
 Object.defineProperty(window, "onewallet", {
   enumerable: true,
   writable: true,
   configurable: true,
-  value: walletController,
+  value: walletProvider,
 });
