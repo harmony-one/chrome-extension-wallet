c0a9b78c10362ed0bd835232c1b0d12e8ad6c837
Raptor <blueraptor1001@gmail.com>
2020-09-30 09:21:26 -0700

    Finish import/export contact feature

--- src/popup/pages/Settings/Contacts/index.vue
+++ src/popup/pages/Settings/Contacts/index.vue
@@ -5,9 +5,15 @@
       <div class="contact-header">
         <button class="primary contact-but" @click="showAddModal">Add</button>
         <div class="import-header">
-          <button class="primary contact-but" @click="importContact">
-            Import
-          </button>
+          <label for="importcontact" class="file-label">Import</label>
+          <input
+            type="file"
+            id="importcontact"
+            name="importcontact"
+            class="file-input"
+            @click="(e) => (e.target.value = null)"
+            @change="importContact"
+          />
           <button class="primary contact-but" @click="exportContact">
             Export
           </button>
@@ -118,11 +124,15 @@
 import { mapState } from "vuex";
 import ContactItem from "./ContactItem";
 import { isValidAddress } from "@harmony-js/utils";
+import { saveAs } from "file-saver";
+import _ from "lodash";
 export default {
   data: () => ({
     editIndex: -1,
     newName: "",
+    newContacts: [],
     newAddress: "",
+    file: null,
   }),
   components: {
     ContactItem,
@@ -223,12 +233,81 @@ export default {
         address: this.newAddress,
       });
     },
-    importContact() {},
-    exportContact() {},
+    async importContact(event) {
+      this.file = event.target.files[0];
+      if (!this.file) {
+        this.$notify({
+          group: "notify",
+          type: "error",
+          text: "Please select the contact file",
+        });
+        return;
+      }
+      this.$store.commit("loading", true);
+
+      const _this = this;
+      await new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          try {
+            _this.newContacts = JSON.parse(e.target.result);
+            resolve();
+          } catch (error) {
+            this.$notify({
+              group: "notify",
+              type: "error",
+              text: "Contact file is invalid",
+            });
+            this.$store.commit("loading", false);
+            return;
+          }
+        };
+        reader.readAsText(this.file);
+      });
+
+      try {
+        this.newContacts.forEach(async (contact) => {
+          if (!contact.name || !contact.address) {
+            this.$notify({
+              group: "notify",
+              type: "error",
+              text: "Contact file is invalid",
+            });
+            this.$store.commit("loading", false);
+            return;
+          }
+          const findContactbyAddress = _.find(this.contacts, {
+            address: contact.address,
+          });
+          if (!findContactbyAddress) {
+            const newContact = {
+              name: this.renameIfExist(contact.name),
+              address: contact.address,
+            };
+            this.$store.dispatch("settings/addContact", newContact);
+          }
+        });
+      } catch (err) {
+        this.$notify({
+          group: "notify",
+          type: "error",
+          text: err.message,
+        });
+        this.$store.commit("loading", false);
+        return;
+      }
+      this.$store.commit("loading", false);
+    },
+    exportContact() {
+      var file = new File([JSON.stringify(this.contacts)], "harmony.hcf", {
+        type: "text/plain;charset=utf-8",
+      });
+      saveAs(file);
+    },
   },
 };
 </script>
-<style scoped>
+<style lang="scss" scoped>
 .contact-but {
   padding: 5px;
 }
@@ -243,5 +322,36 @@ export default {
   display: flex;
   justify-content: space-between;
   margin-bottom: 16px;
+}
+.file-label {
+  text-align: center;
+  font-size: 13px;
+  padding: 4px 10px;
+  font-weight: 400;
+  min-width: 60px;
+  color: white;
+  border-radius: 0.3rem;
+  cursor: pointer;
+  background: #0a93eb;
+  white-space: nowrap;
+  outline: none;
+  border: 2px solid #0a93eb;
+  &:hover {
+    background-color: #0987d7;
+  }
+  &:active {
+    background-color: #1f6bb7;
+  }
+  &:disabled {
+    background: #e0e0e0;
+    border: 2px solid #e0e0e0;
+    color: #888;
+  }
+}
+
+.file-input {
+  opacity: 0;
+  position: absolute;
+  z-index: -1;
 }
 </style>
